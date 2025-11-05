23ce3a565370bdfd3023e35e1c2f6bb09443619f
amontuori1991 <alexmontuori1991@gmail.com>
2025-11-05 22:51:28 +0100

    Aggiunta registrazione con conferma mail + smtp gmail

--- Program.cs
+++ Program.cs
@@ -136,13 +136,8 @@ builder.Services.AddRazorPages(o =>
     o.Conventions.AllowAnonymousToAreaPage("Identity", "/Account/Register");
     o.Conventions.AllowAnonymousToAreaPage("Identity", "/Account/RegisterConfirmation");
     o.Conventions.AllowAnonymousToAreaPage("Identity", "/Account/ConfirmEmail");
-
-    // ✅ Route esplicite (evita sorprese in publish/Linux)
-    o.Conventions.AddAreaPageRoute("Identity", "/Account/Login", "/Identity/Account/Login");
-    o.Conventions.AddAreaPageRoute("Identity", "/Account/Register", "/Identity/Account/Register");
-    o.Conventions.AddAreaPageRoute("Identity", "/Account/RegisterConfirmation", "/Identity/Account/RegisterConfirmation");
-    o.Conventions.AddAreaPageRoute("Identity", "/Account/ConfirmEmail", "/Identity/Account/ConfirmEmail");
 });
+
 
 builder.Services.AddControllersWithViews();
 
