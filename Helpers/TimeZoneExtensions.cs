2145abdfeab7632bb9783769b5f8bbb7ca5ce6da
amontuori1991 <alexmontuori1991@gmail.com>
2026-01-15 16:28:03 +0100

    Orario schedine e comunity

---
+++ Helpers/TimeZoneExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace NextStakeWebApp.Helpers
+{
+    public static class TimeZoneExtensions
+    {
+        private static readonly TimeZoneInfo RomeTz = GetRomeTimeZone();
+
+        private static TimeZoneInfo GetRomeTimeZone()
+        {
+            // Linux (Render): "Europe/Rome"
+            // Windows (dev): "W. Europe Standard Time"
+            try { return TimeZoneInfo.FindSystemTimeZoneById("Europe/Rome"); }
+            catch
+            {
+                return TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time");
+            }
+        }
+
+        public static DateTime ToRomeTime(this DateTime utc)
+        {
+            // Se dal DB arriva Kind=Unspecified, lo forziamo a UTC perché i tuoi campi sono *Utc*
+            var asUtc = utc.Kind == DateTimeKind.Utc
+                ? utc
+                : DateTime.SpecifyKind(utc, DateTimeKind.Utc);
+
+            return TimeZoneInfo.ConvertTimeFromUtc(asUtc, RomeTz);
+        }
+
+        public static DateTime? ToRomeTime(this DateTime? utc)
+        {
+            if (!utc.HasValue) return null;
+            return utc.Value.ToRomeTime();
+        }
+    }
+}
