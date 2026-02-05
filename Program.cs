e679fa697064a6c616c866bc975ec6160b81bfc1
amontuori1991 <a.montuori@azerouno.it>
2026-02-05 14:14:36 +0100

    Riattivazione notifiche e live

--- Program.cs
+++ Program.cs
@@ -180,7 +180,7 @@ builder.Services.AddRazorPages(o =>
 
 
 builder.Services.AddControllersWithViews();
-// builder.Services.AddHostedService<NextStakeWebApp.Services.LiveNotifyWorker>(); // 🛑 DISATTIVATO: worker notifiche live
+builder.Services.AddHostedService<NextStakeWebApp.Services.LiveNotifyWorker>();
 
 
 var app = builder.Build();
